5a34b426582461d1b1cfa05b19936c04512aefe5
apizzimenti <pizzimentianthony@gmail.com>
2018-06-12 01:14:09 -0400

    Basic shapefile intake.
    Author: Anthony Pizzimenti
    Date:   June 12, 2018
    
    A simple function to consume a shapefile (default filepath is that for
    test data), generate an adjacency matrix for that file, and convert it
    to a NetworkX Graph object.

---
+++ rundmcmc/intake.py
@@ -0,0 +1,19 @@
+
+import networkx as nx
+import pysal as ps
+import geopandas as gp
+import matplotlib.pyplot as plt
+
+
+def intake(filepath="./../data/test/testData.shp"):
+    """
+        Reads in a shapefile through PySAL, and generates an
+        adjacency matrix (rook adjacency). Then, load the converted
+        NumPy data from PySAL into NetworkX.
+    """
+    shp = ps.rook_from_shapefile(filepath)
+    return nx.Graph(shp.full()[0])
+
+
+if __name__ == "__main__":
+    intake()
