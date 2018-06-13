a21640f2850327cb0ce056a0d2095a22db53e321
Will Adler <will@wtadler.com>
2018-06-13 12:11:04 -0400

    Get rid of trailing whitespace

--- rundmcmc/ingest.py
+++ rundmcmc/ingest.py
@@ -16,18 +16,18 @@ def ingest(filepath, name_of_geoid_col):
     neighbors = ps.weights.Rook.from_dataframe(df, geom_col="geometry").neighbors
     for n in neighbors:
         neighbors[n] = sorted(neighbors[n])
-        
+
     shared_perims = {}
     for shape in neighbors.keys():
         shared_perims[shape] = []
-        
+
         for n in neighbors[shape]:
             shared_perims[shape].append(
                 df.loc[shape, 'geometry'].intersection(
                     df.loc[n, 'geometry']).length)
-    
+
     sorted_keys = sorted(neighbors)
-    
+
     return ([neighbors[i] for i in sorted_keys],
             [shared_perims[i] for i in sorted_keys],
             [df.loc[i, name_of_geoid_col] for i in sorted_keys])
@@ -35,3 +35,4 @@ def ingest(filepath, name_of_geoid_col):
 
 if __name__ == "__main__":
     ingest("testData/testData.shp", "CD")
+    
