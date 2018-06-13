8c03d173643784b71ef3c74bf66ac263e5e50fb4
Max Hully <max.hully@gmail.com>
2018-06-13 15:43:31 -0400

    Added aggregation of statistics

--- tests/test_partition.py
+++ tests/test_partition.py
@@ -26,3 +26,19 @@ def test_propose_random_flip_proposes_a_dict():
     partition = example_partition()
     proposal = propose_random_flip(partition)
     assert isinstance(proposal, dict)
+
+
+def test_Partition_can_update_stats():
+    graph = networkx.complete_graph(3)
+    assignment = {0: 1, 1: 1, 2: 2}
+
+    graph.nodes[0]['stat'] = 1
+    graph.nodes[1]['stat'] = 2
+    graph.nodes[2]['stat'] = 3
+
+    partition = Partition(graph, assignment, aggregate_fields=['stat'])
+    assert partition.statistics['stat'][2] == 3
+    flip = {1: 2}
+
+    new_partition = partition.merge(flip)
+    assert new_partition.statistics['stat'][2] == 5
