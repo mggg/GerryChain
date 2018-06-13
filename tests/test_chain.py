bdd7081c008a3eceb01dc1aad8a7e5ab4e12f1e3
Max Hully <max.hully@gmail.com>
2018-06-13 19:45:30 -0400

    Used unused variable in test

--- tests/test_chain.py
+++ tests/test_chain.py
@@ -23,6 +23,9 @@ def test_MarkovChain_runs_only_total_steps_times():
     chain = MarkovChain(mock_proposal, mock_is_valid, mock_accept, initial, total_steps=10)
     counter = 0
     for state in chain:
+        assert isinstance(state, MockState)
         if counter >= 10:
             assert False
         counter += 1
+    if counter < 10:
+        assert False
