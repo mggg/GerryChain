b9821ffceabfd70a137ea14c1868a62fd7b063be
apizzimenti <pizzimentianthony@gmail.com>
2018-06-17 00:29:50 -0400

    Re-purposing Logger; Runner replacing Logger.
    Author(s):      Anthony Pizzimenti
    Date:           June 16-17, 2018
    
    The Runner class now effectively serves the same role as the original
    Logger class. It runs the chain and provides meaningful, simple
    facilities to handle custom in-chain output methods, histogram tracking,
    custom binning intervals, and post-chain data visualizations.

---
+++ rundmcmc/Runner.py
@@ -0,0 +1,70 @@
+
+from rundmcmc.Loggers import Loggers
+import matplotlib.pyplot as plt
+
+
+class Runner:
+    def __init__(self, Chain, funcs=None, ends=None, **kwargs):
+        """
+            Runner is a wrapper for the MarkovChain class that tracks
+            statistics as we move through the states of the chain. Uses
+            regular Python lists for performance (as it's faster to append
+            to a linked list than it is to merge two NumPy arrays).
+            :Chain:     Instance of the MarkovChain class.
+            :funcs:     List of functions that log data while the chain runs.
+            :ends:      List of functions that log/save/do whatever with data
+                        after the chain completes.
+            :**kwargs:  Command-line (or extraneous) arguments.
+        """
+        self.Chain = Chain
+        self.Loggers = Loggers(funcs, ends)
+
+        # Set default interval (or take it from additional keyword arguments),
+        # but don't use a ternary logic operator here bc the line would be
+        # too long. Then, if Loggers exist, set the interval to the default
+        # binning rate (1%).
+        self.interval = None
+        
+        if kwargs.get("interval") is None:
+            self.interval = max(1, int(self.Chain.total_steps * 0.01))
+        else:
+            self.interval = int(kwargs["interval"])
+
+        # Find the total number of districts.
+        stats = self.Chain.state
+        first_key = next(iter(stats.keys()))
+        self.num_districts = len(stats[first_key])
+
+        # Initialize histogram, generating lists at each key.
+        self.histograms = {stat: list(cds.values()) for stat, cds in stats.items()}
+
+        # Run the chain.
+        self._run_chain()
+
+    def _run_chain(self):
+        """
+            Class-private method that runs automagically. Runs the chain
+            and, at the specified intervals, bins the current statistics
+            of the partition. This can be augmented to log stats to the
+            console at the same rate, but we can add that as a flag later.
+            Futhermore, this should be modified to control for interval
+            size, as small intervals with a large number of iterations will
+            stress the memory of the machine. We should consider writing
+            to a file (every n intervals) or using another caching method.
+        """
+        step = 0
+
+        # Loop for running the chain. *This method needs to be augmented.*
+        for state in self.Chain:
+            # If we encounter an interval step, get the current statistics
+            # and add their values to the existing histograms. Also passes
+            # the current state to the Loggers object.
+            if step % self.interval == 0:
+                self.Loggers.during_chain(state)
+                for stat in self.histograms:
+                    self.histograms[stat] += list(state[stat].values())
+
+            step += 1
+
+        # Run all the functions designed for when the state has completed.
+        self.Loggers.after_chain(state)
--- rundmcmc/__main__.py
+++ rundmcmc/__main__.py
@@ -1,43 +1,30 @@
 
 from rundmcmc.ingest import ingest
 from rundmcmc.make_graph import construct_graph, get_list_of_data, add_data_to_graph, pull_districts
-from rundmcmc.validity import contiguous, Validator, fast_connected
+from rundmcmc.validity import contiguous, Validator, fast_connected, single_flip_contiguous
 from rundmcmc.partition import Partition, propose_random_flip
 from rundmcmc.chain import MarkovChain
-from rundmcmc.Logger import Logger
+from rundmcmc.Runner import Runner
 from rundmcmc.updaters import statistic_factory
-import time
-
 
 def main():
-    # Time the creation of the dual graph.
-    start = time.time()
-    print("Constructing dual graph.")
     graph = construct_graph(*ingest("./testData/wyoming_test.shp", "GEOID"))
-    end = time.time()
-    print("Done in {} seconds.\n".format(str(end - start)))
 
-    print("Reading in area data and congressional district assignments.")
     cd_data = get_list_of_data('./testData/wyoming_test.shp', ['CD', 'ALAND'])
-    print("Done.\n")
 
-    print("Adding data to the graph.")
     add_data_to_graph(cd_data, graph, ['CD', 'ALAND'])
-    print("Done.\n")
 
-    print("Initializing a Partition, Validator, and updaters.")
     assignment = pull_districts(graph, 'CD')
-    validator = Validator([fast_connected])
+    validator = Validator([single_flip_contiguous])
     updaters = {'area': statistic_factory('ALAND', alias='area')}
 
     initial_partition = Partition(graph, assignment, updaters)
     accept = lambda x: True
-    print("Done. Moving on to the chain.\n")
 
-    # Exposes the chain object to the Logger.
-    return MarkovChain(propose_random_flip, validator, accept, initial_partition, total_steps=2**15)
+    # Exposes the chain object to the Runner.
+    return MarkovChain(propose_random_flip, validator, accept, initial_partition, total_steps=10)
 
 
 if __name__ == "__main__":
-    # Wrap main()'s chain object in the Logger.
-    Logger(main(), console=False)
+    # Wrap main()'s chain object in the Runner.
+    Runner(main())
