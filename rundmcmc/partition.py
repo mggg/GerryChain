d7cd6b39aff132516b733685fda0f1d31eee0208
Robert Dougherty-Bliss <robert.w.bliss@gmail.com>
2018-06-18 15:06:40 -0400

    Update docs and style in minor ways

--- rundmcmc/partition.py
+++ rundmcmc/partition.py
@@ -5,7 +5,8 @@ def propose_random_flip(partition):
     """Proposes a random boundary flip from the partition.
 
     :partition: The current partition to propose a flip from.
-    :return: a dictionary of with the flipped node mapped to its new assignment
+    :returns: a dictionary of with the flipped node mapped to its new assignment
+
     """
     edge = random.choice(tuple(partition['cut_edges']))
     index = random.choice((0, 1))
@@ -17,15 +18,19 @@ def propose_random_flip(partition):
 
 
 class Partition:
-    """Partition represents a partition of the nodes of the graph. It will perform
+    """
+    Partition represents a partition of the nodes of the graph. It will perform
     the first layer of computations at each step in the Markov chain - basic
     aggregations and calculations that we want to optimize.
 
-    :graph: reference to the networkx graph
-    :assignment: dictionary mapping nodes to their assigned parts of the partition
     """
 
     def __init__(self, graph, assignment, updaters=None, fields=None):
+        """
+        :graph: NetworkX graph.
+        :assignment: Dictionary mapping node ids to their partition cell.
+
+        """
         self.graph = graph
         self.assignment = assignment
 
@@ -38,12 +43,14 @@ class Partition:
         self.fields = fields
 
     def merge(self, flips):
-        """Takes a dictionary of new assignments and returns the Partition
-        obtained by applying these new assignments to this instance (self)
-        of Partition.
+        """
+        Takes a dictionary of new assignments and returns the Partition
+        obtained by applying these new assignments to this instance (self) of
+        Partition.
 
         :flips: a dictionary of nodes mapped to their new assignments
-        :return: a new Partition instance
+        :returns: a new Partition instance
+
         """
         new_assignment = {**self.assignment, **flips}
 
