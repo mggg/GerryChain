88b1ff627fb22c333d3c37db791ff784f7316a78
apizzimenti <pizzimentianthony@gmail.com>
2018-06-21 15:03:06 -0400

    First working graph report.

--- rundmcmc/graph_report.py
+++ rundmcmc/graph_report.py
@@ -6,3 +6,18 @@
         • discrete diameter
         • # of faces (by eulerian characteristic)
         • graph density
+"""
+import networkx as nx
+import networkx.algorithms as nxa
+
+
+def graph_report(graph):
+    """
+        Generates a basic report on simple properties of the graph.
+        :graph: NetworkX graph object.
+    """
+    vertices, edges = len(list(nx.nodes(graph))), len(list(nx.edges(graph)))
+    density = nx.density(graph)
+    connectivity = nxa.node_connectivity(graph)
+    return vertices, edges, density, connectivity
+    
--- rundmcmc/initial_report.py
+++ rundmcmc/initial_report.py
@@ -1,8 +1,7 @@
 
 import geopandas as gp
-import pysal as ps
-import networkx as nx
 from rundmcmc.make_graph import construct_graph, add_data_to_graph
+from rundmcmc.graph_report import graph_report
 
 
 def report(prorated=None, location="./testData/mo_dists.geojson"):
@@ -10,7 +9,6 @@ def report(prorated=None, location="./testData/mo_dists.geojson"):
         Takes in a geopandas dataframe of prorated voting data or the location
         of a shape/geojson file, generates a graph, and runs the initial report
         functions.
-
         :prorated: A dataframe of prorated data.
         :location: Filepath for a shape/geojson/csv file.
     """
@@ -19,6 +17,7 @@ def report(prorated=None, location="./testData/mo_dists.geojson"):
     add_data_to_graph(df, graph, list(df), id_col="GEOID10")
 
     ### RUN ALL THE REPORTING THINGS! ###
+    print(graph_report(graph))
 
 if __name__ == "__main__":
     report()
