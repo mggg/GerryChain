491e355e8eb6cdbd085a45e800032d4a60a835e4
Max Hully <max.hully@gmail.com>
2018-06-27 16:32:40 -0400

    Computed some PA runs!

--- rundmcmc/__main__.py
+++ rundmcmc/__main__.py
@@ -1,26 +1,50 @@
+import functools
+
+import matplotlib.pyplot as plt
+
 from rundmcmc.defaults import BasicChain, PA_partition
-from rundmcmc.scores import mean_median, mean_thirdian, get_dict_of_flips
-
-
-def print_summary(partition, scores):
-    print("")
-    print(f"Polsby-Popper: {partition['polsby_popper']}")
+from rundmcmc.run import pipe_to_table
+from rundmcmc.scores import (compute_meta_graph_degree, efficiency_gap,
+                             mean_median, mean_thirdian)
+from rundmcmc.validity import L1_reciprocal_polsby_popper
 
 
 def main():
     initial_partition = PA_partition()
 
     chain = BasicChain(initial_partition, total_steps=10)
+    print("About to compute metagraph degree")
+
+    compute_meta_graph_degree(chain)
+
+    chain = BasicChain(initial_partition, total_steps=10000)
 
     scores = {
-        'Mean-Median': mean_median,
-        'Mean-Thirdian': mean_thirdian
+        'Mean-Median': functools.partial(mean_median, proportion_column_name='VoteA%'),
+        'Mean-Thirdian': functools.partial(mean_thirdian, proportion_column_name='VoteA%'),
+        'Efficiency Gap': functools.partial(efficiency_gap, col1='VoteA', col2='VoteB'),
+        'L1 Reciprocal Polsby-Popper': L1_reciprocal_polsby_popper
     }
 
-    for partition in chain:
-        print_summary(partition, scores)
-        print(chain.counter)
-    get_dict_of_flips(chain)
+    initial_scores = {key: score(initial_partition) for key, score in scores.items()}
+
+    table = pipe_to_table(chain, scores)
+
+    fig, axes = plt.subplots(2, 2)
+
+    quadrants = {
+        'Mean-Median': (0, 0),
+        'Mean-Thirdian': (0, 1),
+        'Efficiency Gap': (1, 0),
+        'L1 Reciprocal Polsby-Popper': (1, 1)
+    }
+
+    for key in scores:
+        quadrant = quadrants[key]
+        axes[quadrant].hist(table[key], bins=50)
+        axes[quadrant].set_title(key)
+        axes[quadrant].axvline(x=initial_scores[key], color='r')
+    plt.show()
 
 
 if __name__ == "__main__":
--- rundmcmc/proposals.py
+++ rundmcmc/proposals.py
@@ -22,6 +22,70 @@ def propose_random_flip(partition):
     return flip
 
 
+def propose_several_random_flips(partition):
+    number_of_flips = random.randint(2, 7)
+
+    proposal = dict()
+
+    for i in range(number_of_flips):
+        proposal.update(propose_random_flip(partition))
+
+    return proposal
+
+
+def propose_flip_every_district(partition):
+    proposal = dict()
+
+    for dist_edges in partition['cut_edges_by_part'].values():
+        edge = random.choice(list(dist_edges))
+
+        index = random.choice((0, 1))
+        flipped_node, other_node = edge[index], edge[1 - index]
+        flip = {flipped_node: partition.assignment[other_node]}
+
+        proposal.update(flip)
+
+    return proposal
+
+
+def propose_chunk_flip(partition):
+    proposal = dict()
+
+    edge = random.choice(tuple(partition['cut_edges']))
+    index = random.choice((0, 1))
+
+    flipped_node = edge[index]
+
+    valid_flips = [nbr for nbr in partition.graph.neighbors(
+        flipped_node) if partition.assignment[nbr] != partition.assignment[flipped_node]]
+
+    for flipped_neighbor in valid_flips:
+        proposal.update({flipped_neighbor: partition.assignment[flipped_node]})
+
+    return proposal
+
+
+def propose_flip_every_edge_of_district(partition):
+    proposal = dict()
+
+    edges = random.choice(list(partition['cut_edges_by_part'].values()))
+
+    for edge in edges:
+        if(random.random() > .5):
+            index = random.choice((0, 1))
+            flipped_node, other_node = edge[index], edge[1 - index]
+            proposal.update({flipped_node: partition.assignment[other_node]})
+
+    return proposal
+
+
+def propose_single_or_chunk(partition):
+    if(random.random() > .1):
+        return propose_random_flip(partition)
+    else:
+        return propose_chunk_flip(partition)
+
+
 def number_of_flips(partition, dict_of_flips, prev_partition):
     flips = partition.flips
     if flips is None or flips is prev_partition:
