d42d42d3dd823cb7a3f09f35bd16698d22f41bea
maryb <marybarker103@gmail.com>
2018-06-29 13:20:54 -0400

    cleaned up parse_config some, added dependency checker for updaters, and added more output/handler types

--- rundmcmc/__main__.py
+++ rundmcmc/__main__.py
@@ -2,13 +2,13 @@ from rundmcmc.parse_config import read_basic_config
 
 
 def main():
-    chain, chain_func, scores, output_func = read_basic_config("basic_config.ini")
+    chain, chain_func, scores, output_func, output_type = read_basic_config("basic_config.ini")
     print("setup the chain")
 
     output = chain_func(chain)
     print("ran the chain")
 
-    output_func(output, scores)
+    output_func(output, scores, output_type)
 
 
 if __name__ == "__main__":
--- rundmcmc/parse_config.py
+++ rundmcmc/parse_config.py
@@ -1,53 +1,30 @@
 import functools
 import configparser
 
-import matplotlib.pyplot as plt
-
 import rundmcmc.make_graph as mgs
 import rundmcmc.validity as valids
 import rundmcmc.updaters as updates
 import rundmcmc.scores as scores
 import rundmcmc.proposals as proposals
 import rundmcmc.accept as accepts
+import rundmcmc.output as outputs
 
 from rundmcmc.partition import Partition
 from rundmcmc.chain import MarkovChain
-from rundmcmc.run import pipe_to_table
+from rundmcmc.run import pipe_to_table, flips_to_dict
 
 
-def outputfunc(table, scores):
-    """Function that processes the output of a chain run.
-    (should probably be in a different file at some point
-    in the future)
 
-    outputs a window plot of histograms of logged scores
-    """
-    numrows = 2
-    numcols = int(len(scores) / numrows)
-    numrows = max(numrows, 1)
-    numcols = max(numcols, 1)
-    fig, axes = plt.subplots(ncols=numcols, nrows=numrows)
-
-    scoreNames = [x for x in scores.keys()][: numrows * numcols]
-    quadrants = {
-        key: (int(i / numcols), i % numcols)
-        for i, key in enumerate(scoreNames)
-    }
-
-    initial_scores = table[0]
-
-    for key in scores:
-        quadrant = quadrants[key]
-        axes[quadrant].hist(table[key], bins=50)
-        axes[quadrant].set_title(key)
-        axes[quadrant].axvline(x=initial_scores[key], color='r')
-    plt.show()
+def nothingFunc(args):
+    pass
 
 
-def sLogType(typestr):
+def scoresLogType(typestr):
     """return the type of logger to use for scores"""
     if typestr == "pipe_to_table":
         return pipe_to_table
+    elif typestr == "flips_to_dict":
+        return flips_to_dict
     else:
         print("ERROR: TYPE OF LOGGER NOT SUPPORTED")
 
@@ -90,8 +67,18 @@ def required_graph_fields():
     return ['id', 'pop', 'area', 'cd']
 
 
-def gsource_gdata(configGraphSource, configGraphData):
+def gsource_gdata(config, graphSource, graphData):
     """Create a graph from the config file GRAPH_SOURCE and GRAPH_DATA sections"""
+
+    # make sure the config file has graph information in it
+    if (not config.has_section(graphData)) or (not config.has_section(graphSource)):
+        raise Exception("ERROR: config needs a GRAPH_DATA section and a GRAPH_SOURCE section")
+    if not all(x in list(config[graphData].keys()) for x in required_graph_fields()):
+        elements = " ".join(required_graph_fields())
+        raise Exception("ERROR: graph_data must contain all of the following fields: %s" % elements)
+    configGraphData = config[graphData]
+    configGraphSource = config[graphSource]
+
     ID = configGraphData['id']
     POP = configGraphData['pop']
     AREA = configGraphData['area']
@@ -101,13 +88,96 @@ def gsource_gdata(configGraphSource, configGraphData):
     return graph, POP, AREA, CD
 
 
-def vsource_vdata(graph, configVoteSource, configVoteData):
+def vsource_vdata(graph, config, voteSource, voteData):
     """Add data to graph from the config file VOTE_SOURCE and VOTE_DATA sections"""
+    if not config.has_section(voteSource):
+        return
+
+    configVoteSource = config[voteSource]
+    configVoteData = config[voteData]
     source = configVoteSource['vSource']
     geoid = configVoteSource['vSourceID']
+
     cols_to_add = [x for x in configVoteData.values()]
     mdata = mgs.get_list_of_data(source, cols_to_add, geoid)
     mgs.add_data_to_graph(mdata, graph, cols_to_add, geoid)
+
+    return [x for x in configVoteData.values()]
+
+
+def escores_edata(config, evalScores, evalScoresData):
+    eval_scores = ''
+    outputFileName = None
+    scoreVisType = None
+    chainfunc = nothingFunc
+
+    if config.has_section('EVALUATION_SCORES'):
+        eval_list = config['EVALUATION_SCORES'].values()
+        funcs, cols = zip(*[ (x.split(',')[0], x.split(',')[1:]) for x in eval_list])
+
+        eval_scores = {funcs[x]: scores_arg_placement(funcs[x], cols[x]) for x in range(len(funcs))}
+
+        if config.has_section('EVALUATION_SCORES_DATA'):
+            if 'evalscorelogtype' in list(config['EVALUATION_SCORES_DATA'].keys()):
+                scoreLogType = scoresLogType(config['EVALUATION_SCORES_DATA']['evalscorelogtype'])
+                chainfunc = functools.partial(scoreLogType, handlers=eval_scores)
+
+            if 'vistype' in list(config['EVALUATION_SCORES_DATA'].keys()):
+                scoreVisType = getattr(outputs, config['EVALUATION_SCORES_DATA']['vistype'])
+
+            if 'savefilename' in list(config['EVALUATION_SCORES_DATA'].keys()):
+                outputFileName = config['EVALUATION_SCORES_DATA']['savefilename']
+
+    return eval_scores, chainfunc, funcs, scoreVisType, outputFileName
+
+
+def dependencies(scoreType, POP, AREA):
+    """returns the updaters dependencies for whatever scoretype
+    is passed in. To be used as a checking device against the
+    user-specified validators and score functions
+    NOTE: this does not check efficiency/mean-median/mean-thirdian.
+
+    :scoreType: name of function to check dependencies of
+    :POP: name of population column in graph object
+    :AREA: name of area column in graph object
+
+    """
+    depends = {}
+    if scoreType == "areas":
+        depends = {"areas":updates.Tally(AREA, alias="areas")}
+
+    elif scoreType == "population":
+        depends = {"population": updates.Tally(POP, alias="population")}
+
+    elif scoreType == "perimeters":
+        depends = {
+                'boundary_nodes': updates.boundary_nodes,
+                'cut_edges': updates.cut_edges,
+                'cut_edges_by_part': updates.cut_edges_by_part,
+                'exterior_boundaries': updates.exterior_boundaries,
+                'perimeters': updates.perimeters
+                }
+
+    elif scoreType == "polsby_popper":
+        depends = {**dependencies("areas", POP, AREA),**dependencies("perimeters", POP, AREA)}
+        depends["polsby_popper"] = updates.polsby_popper_updater
+
+    elif scoreType == "L1_reciprocal_polsby_popper":
+        depends = dependencies("polsby_popper", POP, AREA)
+
+    elif scoreType == "no_vanishing_districts":
+        depends = dependencies("population", POP, AREA)
+
+    elif scoreType == "fast_connected":
+        depends = {}
+
+    elif scoreType == "within_percent_of_ideal_population":
+        depends = dependencies("population", POP, AREA)
+
+    elif scoreType == "p_value":
+        depends = {"mean_median": scores.mean_median, "mean_thirdian": scores.mean_thirdian}
+
+    return depends
 
 
 def read_basic_config(configFileName):
@@ -122,86 +192,56 @@ def read_basic_config(configFileName):
     config.read(configFileName)
 
     # SET UP GRAPH AND PARTITION SECTION
-    # make sure the config file has graph information in it
-    if (not config.has_section('GRAPH_DATA')) or (not config.has_section('GRAPH_SOURCE')):
-        raise Exception("ERROR: config needs a GRAPH_DATA section and a GRAPH_SOURCE section")
-    if not all(x in list(config['GRAPH_DATA'].keys()) for x in required_graph_fields()):
-        elements = " ".join(required_graph_fields())
-        raise Exception("ERROR: graph_data must contain all of the following fields:%s" % elements)
-
     # create graph and get global names for required graph attributes
-    graph, POP, AREA, CD = gsource_gdata(config['GRAPH_SOURCE'], config['GRAPH_DATA'])
-    # if there is more data to add to graph (e.g. voting data in a csv)
-    if config.has_section('VOTE_DATA_SOURCE'):
-        vsource_vdata(graph, config['VOTE_DATA_SOURCE'], config['VOTE_DATA'])
-        vlist = [x for x in config['VOTE_DATA'].values()]
-
+    graph, POP, AREA, CD = gsource_gdata(config, 'GRAPH_SOURCE', 'GRAPH_DATA')
+    voteDataList = vsource_vdata(graph, config, 'VOTE_DATA_SOURCE', 'VOTE_DATA')
+    # create a list of vote columns to update
+    DataUpdaters = {v:updates.Tally(v) for v in voteDataList}
     # construct initial districting plan
     assignment = {x[0]: x[1][CD] for x in graph.nodes(data=True)}
-
     # set up validator functions and create Validator class instance
-    validators = [valids.fast_connected]
+    validatorsUpdaters = []
     if config.has_section('VALIDITY') and len(list(config['VALIDITY'].keys())) > 0:
         validators = [getattr(valids, x) for x in config['VALIDITY'].values()]
+        validatorsUpdaters.extend(list(config['VALIDITY'].values()))
     validators = valids.Validator(validators)
-
-    # set up baseline updaters for running chain with polsby-popper and other metrics
-    updaters = {'population': updates.Tally(POP, alias='population'),
-            'perimeters': updates.perimeters,
-            'exterior_boundaries': updates.exterior_boundaries,
-            'boundary_nodes': updates.boundary_nodes,
-            'cut_edges': updates.cut_edges,
-            'cut_edges_by_part': updates.cut_edges_by_part,
-            'polsby_popper': updates.polsby_popper_updater,
-            'areas': updates.Tally(AREA, alias="areas")}
-
-    for v in vlist:
-        updaters[v] = updates.Tally(v)
-    # create partition
-    initial_partition = Partition(graph, assignment, updaters)
+    # add updaters required by this list of validators to list of updaters
+    for x in validatorsUpdaters:
+        DataUpdaters.update(dependencies(x, POP, AREA))
     # END SET UP GRAPH AND PARTITION SECTION
 
     # SET UP MARKOVCHAIN RUN SECTION
     # set up parameters for markovchain run
     chainparams = config['MARKOV_CHAIN']
-
     # number of steps to run
     num_steps = 1000
     if 'num_steps' in list(chainparams.keys()):
         num_steps = int(chainparams['num_steps'])
-
     # type of flip to use
     proposal = proposals.propose_random_flip
     if 'proposal' in list(chainparams.keys()):
         proposal = getattr(proposals, chainparams['proposal'])
-
     # acceptance function to use
     accept = accepts.always_accept
     if 'accept' in list(chainparams.keys()):
         accept = getattr(accepts, chainparams['accept'])
-
-    # create markovchain instance
-    chain = MarkovChain(proposal, validators, accept, initial_partition, num_steps)
     # END SET UP MARKOVCHAIN RUN SECTION
 
     # SET UP DATA PROCESSOR FOR CHAIN RUN
-
     # get evaluation scores to compute and the columns to use for each
-    eval_scores = ''
-    if config.has_section('EVALUATION_SCORES'):
-        eval_list = config['EVALUATION_SCORES'].values()
-        eval_scores = {x.split(',')[0]:
-                scores_arg_placement(x.split(',')[0], x.split(',')[1:]) for x in eval_list}
-        if config.has_section('EVALUATION_SCORES_DATA'):
-            scoreLogType = sLogType(config['EVALUATION_SCORES_DATA']['evalScoreLogType'])
-            chainfunc = functools.partial(scoreLogType, handlers=eval_scores)
-        else:
-            def chainfunc(thing):
-                pass
-    else:
-        def chainfunc(thing):
-            pass
+    escores, cfunc, elist, sVisType, outFName = escores_edata(config,
+            "EVALUATION_SCORES",
+            "EVALUATION_SCORES_DATA")
+
+    # add evaluation scores updaters to list of updators
+    for x in elist:
+        DataUpdaters.update(dependencies(x, POP, AREA))
 
     # END SET UP DATA PROCESSOR FOR CHAIN RUN
+    updaters = DataUpdaters
 
-    return chain, chainfunc, eval_scores, outputfunc
+    # create markovchain instance
+    initial_partition = Partition(graph, assignment, updaters)
+    chain = MarkovChain(proposal, validators, accept, initial_partition, num_steps)
+
+    return chain, cfunc, escores, sVisType, outFName
