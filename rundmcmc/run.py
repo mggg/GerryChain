1f1b565e39c8af7136299cc5e8f2f86516d40435
Max Hully <max.hully@gmail.com>
2018-06-30 15:52:52 -0400

    Made a periodic flips report

--- rundmcmc/run.py
+++ rundmcmc/run.py
@@ -23,6 +23,27 @@ def run(chain, loggers):
     return [logger.after(chain.state) for logger in loggers]
 
 
+class PeriodicFlipsReport:
+    def __init__(self, cadence=100):
+        self.cadence = cadence
+        self.counter = 0
+
+    def __call__(self, partition):
+        if self.counter % self.cadence == 0:
+            # Then we just returned a report, so we'll reset flips_since_last_report
+            self.flips_since_last_report = dict()
+
+        self.counter += 1
+
+        if partition.flips:
+            self.flips_since_last_report = {**self.flips_since_last_report, **partition.flips}
+
+        if self.counter % self.cadence == 0:
+            return self.flips_since_last_report
+        else:
+            return None
+
+
 def handle_chain(chain, handlers):
     for state in chain:
         yield {key: handler(state) for key, handler in handlers.items()}
