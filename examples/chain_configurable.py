6422f26ef6d9d0adf1a64c010e05068ab313580b
Robert Dougherty-Bliss <robert.w.bliss@gmail.com>
2018-07-06 17:51:27 -0400

    Cut long lines

--- examples/chain_configurable.py
+++ examples/chain_configurable.py
@@ -1,4 +1,5 @@
-from rundmcmc.validity import Validator, no_vanishing_districts, refuse_new_splits, single_flip_contiguous
+from rundmcmc.validity import (Validator, no_vanishing_districts,
+                               refuse_new_splits, single_flip_contiguous)
 from rundmcmc.proposals import propose_random_flip
 from rundmcmc.make_graph import construct_graph
 from rundmcmc.accept import always_accept
@@ -19,7 +20,8 @@ updaters = {'cut_edges': cut_edges}
 initial_partition = Partition(graph, assignment, updaters)
 
 validator = Validator([refuse_new_splits, no_vanishing_districts, single_flip_contiguous])
-chain = MarkovChain(propose_random_flip, validator, always_accept, initial_partition, total_steps=steps)
+chain = MarkovChain(propose_random_flip, validator, always_accept,
+                    initial_partition, total_steps=steps)
 
 for i, partition in enumerate(chain):
     print("{}/{}".format(i + 1, steps))
