d1cfa3d9e4bb3e33af1fa0174e4db78f443d06b5
Robert Dougherty-Bliss <robert.w.bliss@gmail.com>
2018-07-10 12:13:27 -0400

    Add metagraph degree updater

--- rundmcmc/updaters/__init__.py
+++ rundmcmc/updaters/__init__.py
@@ -6,8 +6,10 @@ from .cut_edges import cut_edges, cut_edges_by_part
 from .election import votes_updaters
 from .flows import flows_from_changes
 from .tally import Tally
+from .metagraph_degree import MetagraphDegree
 
 __all__ = ['flows_from_changes', 'votes_updaters', 'polsby_popper',
-    'county_splits', 'cut_edges', 'cut_edges_by_part', 'Tally',
-    'boundary_nodes', 'flips', 'perimeters', 'exterior_boundaries',
-    'interior_boundaries', 'exterior_boundaries_as_a_set', 'CountySplit']
+           'county_splits', 'cut_edges', 'cut_edges_by_part', 'Tally',
+           'boundary_nodes', 'flips', 'perimeters', 'exterior_boundaries',
+           'interior_boundaries', 'exterior_boundaries_as_a_set', 'CountySplit',
+           'MetagraphDegree']
