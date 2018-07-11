9d3ec99b905d4a9ac0cbeba09a7f2d6be7985f7c
drdeford <ddeford@math.dartmouth.edu>
2018-07-11 16:42:17 -0400

    Added trace plots and super main file

--- rundmcmc/vis_output.py
+++ rundmcmc/vis_output.py
@@ -38,3 +38,41 @@ def hist_of_table_scores(table, scores=None, outputFile="output.png",
         plt.savefig(outputFile)
     else:
         plt.show()
+
+
+def trace_of_table_scores(table, scores=None, outputFile="output.png"):
+    """Creates a histogram of each score in scores, where
+    table is keyed on score and has values that can be binned
+
+    outputs a window plot of histograms of logged scores
+    """
+    if not scores:
+        scores = table.keys()
+    numrows = min(2, len(scores))
+    numcols = int(len(scores) / numrows)
+    numrows = max(numrows, 1)
+    numcols = max(numcols, 1)
+    _, axes = plt.subplots(nrows=numrows, ncols=numcols, squeeze=False)
+    plt.subplots_adjust(wspace=.2, hspace=.4)
+
+    if numcols == 1:
+        axes = axes.flatten()
+        quadrants = {key: i for i, key in enumerate(scores.keys())}
+    else:
+        quadrants = {
+            key: (i % numcols, int(i / numcols))
+            for i, key in enumerate(scores.keys()) if i < numrows * numcols
+        }
+
+    initial_scores = table[0]
+
+    for i, key in enumerate(scores.keys()):
+        if i < numrows * numcols:
+            quadrant = quadrants[key]
+            axes[quadrant].plot(table[key])
+            axes[quadrant].set_title(key)
+            axes[quadrant].axhline(y=initial_scores[key], color='r')
+    if outputFile:
+        plt.savefig(outputFile)
+    else:
+        plt.show()
