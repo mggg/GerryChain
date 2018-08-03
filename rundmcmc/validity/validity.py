5fafc72060fddc7f1a230042743be685e71b4808
drdeford <ddeford@math.dartmouth.edu>
2018-08-03 08:14:09 -0400

    L1 and L2 PP score, updated init

--- rundmcmc/validity/validity.py
+++ rundmcmc/validity/validity.py
@@ -46,6 +46,14 @@ def L1_reciprocal_polsby_popper(partition):
     return sum(1 / value for value in partition['polsby_popper'].values())
 
 
+def L1_polsby_popper(partition):
+    return sum(value for value in partition['polsby_popper'].values())
+
+
+def L2_polsby_popper(partition):
+    return math.sqrt(sum(value**2 for value in partition['polsby_popper'].values()))
+
+
 def L_minus_1_polsby_popper(partition):
     return len(partition.parts) / sum(1 / value for value in partition['polsby_popper'].values())
 
