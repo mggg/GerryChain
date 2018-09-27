f3b448b431353730f3fefc3a903f3f1b4c4ab904
Max Hully <max.hully@gmail.com>
2018-09-27 11:23:15 -0400

    Whoops

--- rundmcmc/partition/geographic.py
+++ rundmcmc/partition/geographic.py
@@ -16,12 +16,12 @@ class GeographicPartition(Partition):
         'cut_edges_by_part': cut_edges_by_part
     }
 
-    @classmethod
-    def from_file(cls, filename, columns_to_tally=None):
-        raise NotImplementedError
+    # @classmethod
+    # def from_file(cls, filename, assignment, columns_to_tally=None):
+    #     raise NotImplementedError
 
-        # if isinstance(assignment, str):
-        #     assignment = {node: graph.nodes[node][assignment]
-        #                   for node in graph.nodes}
-        # elif not isinstance(assignment, dict):
-        #     raise TypeError("Assignment must be a dict or a node attribute key")
+    #     # if isinstance(assignment, str):
+    #     #     assignment = {node: graph.nodes[node][assignment]
+    #     #                   for node in graph.nodes}
+    #     # elif not isinstance(assignment, dict):
+    #     #     raise TypeError("Assignment must be a dict or a node attribute key")
