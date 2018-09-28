489ce3685fccff56472fe7ef11951c939dee8069
Max Hully <max.hully@gmail.com>
2018-09-28 16:45:25 -0400

    Add a couple names to the global package namespace

--- rundmcmc/__init__.py
+++ rundmcmc/__init__.py
@@ -1,4 +1,7 @@
 from ._version import get_versions
+from .partition import Partition
+from .chain import MarkovChain
+from .updaters.election import Election
 
 __version__ = get_versions()['version']
 del get_versions
