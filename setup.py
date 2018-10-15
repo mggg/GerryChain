6e7da29e31914983c439e73e678eb4435f13ace2
Max Hully <max.hully@gmail.com>
2018-10-15 12:20:24 -0400

    Add descriptions

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from setuptools import find_packages, setup
 
 import versioneer
 
+with open("./README.rst") as f:
+    long_description = f.read()
+
 requirements = [
     # package requirements go here
     "pandas",
@@ -12,9 +15,11 @@ requirements = [
 
 setup(
     name="GerryChain",
-    description="Short description",
+    description="Use Markov chain Monte Carlo to analyze districting plans and gerrymanders",
     author="Metric Geometry and Gerrymandering Group",
     author_email="gerrymandr@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
     url="https://github.com/mggg/GerryChain",
     packages=find_packages(exclude=("tests",)),
     version=versioneer.get_version(),
