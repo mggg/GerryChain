d623cdb7c39943650df0ab20e5be42103a445d76
Max Hully <max.hully@gmail.com>
2018-12-03 07:23:06 -0500

    Refactor with generators

--- gerrychain/graph/adjacency.py
+++ gerrychain/graph/adjacency.py
@@ -11,35 +11,59 @@ def neighbors(df, adjacency):
     return adjacencies[adjacency](df.geometry)
 
 
-def neighboring_geometries(geometries):
+def str_tree(geometries):
+    """Add ids to geometries and create a STR tree for spatial indexing.
+    Use this for all spatial operations!
+    """
     for i in geometries.index:
         geometries[i].id = i
-
     try:
         tree = STRtree(geometries)
     except AttributeError:
         tree = STRtree(geometries)
+    return tree
 
-    def get_neighbors(geometry):
+
+def neighboring_geometries(geometries, tree=None):
+    """Generator yielding tuples of the form (id, (ids of neighbors)).
+    """
+    if tree is None:
+        tree = str_tree(geometries)
+
+    for geometry in geometries:
         possible = tree.query(geometry)
-        actual = [p for p in possible if (not p.is_empty) and p.id != geometry.id]
-        return actual
+        actual = tuple(
+            p.id for p in possible if (not p.is_empty) and p.id != geometry.id
+        )
+        yield (geometry.id, actual)
 
-    return geometries.apply(get_neighbors)
+
+def intersections_with_neighbors(geometries):
+    """Generator yielding tuples of the form (id, {neighbor_id: intersection}).
+    The intersections may be empty!
+    """
+    for i, neighbors in neighboring_geometries(geometries):
+        intersections = {
+            j: geometries[i].intersection(geometries[j]) for j in neighbors
+        }
+        yield (i, intersections)
 
 
 def queen(geometries):
-    neighbors = neighboring_geometries(geometries)
+    """Return queen adjacency dictionary for the given collection of polygons."""
+    # TODO: Handle overlaps!
     return {
         i: {
-            neighbor.id: {"shared_perim": geometries[i].intersection(neighbor).length}
-            for neighbor in neighbors[i]
+            j: {"shared_perim": intersection.length}
+            for j, intersection in intersections.items()
+            if not intersection.is_empty
         }
-        for i in geometries.index
+        for i, intersections in intersections_with_neighbors(geometries)
     }
 
 
 def rook(geometries):
+    """Return rook adjacency dictionary for the given collection of polygons."""
     return {
         i: {j: data for j, data in neighbors.items() if data["shared_perim"] > 0}
         for i, neighbors in queen(geometries).items()
