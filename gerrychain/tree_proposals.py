46b418feedfa4ce299ea5ab8b119da9d757d999c
Max Hully <max.hully@gmail.com>
2018-12-07 15:05:34 -0500

    Merging in the basic recom proposal

--- gerrychain/tree_proposals.py
+++ gerrychain/tree_proposals.py
@@ -1,150 +1,35 @@
 import random
 
-import networkx as nx
-
-from proposals import propose_random_flip
-from tree_methods import (
-    get_spanning_tree_k,
-    get_spanning_tree_u_w,
-    partition_spanning_tree_all,
-    partition_spanning_tree_single,
-    propose_merge2_tree,
-    recursive_tree_full,
-    tree_cycle_walk_all,
-    tree_cycle_walk_cut,
-    tree_part2,
-)
-
-# Mixed proposals
+from tree_methods import tree_part2
 
 
-def tree_mixed_proposal(partition, p):
-    """ Do an ent step with probabiliity p
-    otherwise single edge flip
-    """
-    if random.random() < p:
-        flips = propose_bounce_single_cut(partition)
-    else:
-        flips = propose_random_flip(partition)
+def recom(partition, pop_col, pop_target, epsilon, node_repeats):
+    """ReCom proposal.
 
-    return flips
+    Description from MGGG's 2018 Virginia House of Delegates report:
+    At each step, we (uniformly) randomly select a pair of adjacent districts and
+    merge all of their blocks in to a single unit. Then, we generate a spanning tree
+    for the blocks of the merged unit with the Kruskal/Karger algorithm. Finally,
+    we cut an edge of the tree at random, checking that this separates the region
+    into two new districts that are population balanced.
 
+    Example usage::
 
-def merge_mixed_proposal(partition, p):
-    """ Do an ent step with probabiliity p
-    otherwise single edge flip
-    """
+        from functools import partial
+        from gerrychain import MarkovChain
+        from gerrychain.tree_proposals import recom
 
-    if random.random() < p:
-        flips = propose_merge2_tree(partition)
-    else:
-        flips = propose_random_flip(partition)
+        # ...define constraints, accept, partition, total_steps here...
 
-    return flips
+        # Ideal population:
+        pop_target = sum(partition["population"].values()) / len(partition)
 
+        proposal = partial(
+            recom, pop_col="POP10", pop_target= epsilon=.05, node_repeats=10
+        )
 
-#  Full-Tree Methods
+        chain = MarkovChain(proposal, constraints, accept, partition, total_steps)
 
-
-def propose_allk_tree_k(partition, pop_col, epsilon, node_repeats):
-    """Build a Karger tree and then partition it.
-    """
-    tree = nx.Graph()
-    tree.add_edges_from(list(get_spanning_tree_k(partition.graph)))
-
-    clusters = recursive_tree_full(
-        partition, tree, len(partition.parts), pop_col, epsilon, node_repeats
-    )
-
-    return clusters
-
-
-def propose_allk_tree_u(partition, pop_col, epsilon, node_repeats):
-    """Build a unifrom tree and then partition it.
-    """
-    tree = nx.Graph()
-    tree.add_edges_from(list(get_spanning_tree_u_w(partition.graph)))
-
-    clusters = recursive_tree_full(
-        partition, tree, len(partition.parts), pop_col, epsilon, node_repeats
-    )
-
-    return clusters
-
-
-# These are different experiments for the ent walk
-
-
-def propose_bounce_single_all(partition, pop_col, epsilon, node_repeats):
-    """Ent walk that only adds enough cut edges
-    and then adds an arbitrary graph edge
-    """
-
-    tree = partition_spanning_tree_single(partition)
-
-    tree = tree_cycle_walk_all(partition, tree)
-
-    clusters = recursive_tree_full(
-        partition, tree, len(partition.parts), pop_col, epsilon, node_repeats
-    )
-
-    return clusters
-
-
-def propose_bounce_single_cut(partition, pop_col, epsilon, node_repeats):
-    """Ent walk that only adds enough cut edges
-    and then adds an arbitrary cut edge
-    """
-
-    tree = partition_spanning_tree_single(partition)
-
-    tree = tree_cycle_walk_cut(partition, tree)
-
-    clusters = recursive_tree_full(
-        partition, tree, len(partition.parts), pop_col, epsilon, node_repeats
-    )
-
-    return clusters
-
-
-def propose_bounce_allcut_all(partition, pop_col, epsilon, node_repeats):
-    """Ent walk that only adds all cut edges
-    and then adds an arbitrary edge
-    """
-
-    tree = partition_spanning_tree_all(partition)
-
-    tree = tree_cycle_walk_all(partition, tree)
-
-    clusters = recursive_tree_full(
-        partition, tree, len(partition.parts), pop_col, epsilon, node_repeats
-    )
-
-    return clusters
-
-
-def propose_bounce_allcut_cut(partition, pop_col, epsilon, node_repeats):
-    """Ent walk that only adds all cut edges
-    and then adds an arbitrary cut edge
-    """
-
-    tree = partition_spanning_tree_all(partition)
-
-    tree = tree_cycle_walk_cut(partition, tree)
-
-    clusters = recursive_tree_full(
-        partition, tree, len(partition.parts), pop_col, epsilon, node_repeats
-    )
-
-    return clusters
-
-
-# Merge Proposal
-
-
-def propose_merge2_tree_partial(partition, pop_col, pop_target, epsilon, node_repeats):
-    """Partial ReCom walk for functools Partial
-    merge_prop = partial(propose_merge2_tree_partial,pop_col="POP10",epsilon=.05,node_repeats=10)
     """
     edge = random.choice(tuple(partition["cut_edges"]))
     parts_to_merge = (partition.assignment[edge[0]], partition.assignment[edge[1]])
@@ -166,14 +51,27 @@ def propose_merge2_tree_partial(partition, pop_col, pop_target, epsilon, node_re
 
 
 def recursive_tree_part(graph, parts, pop_target, pop_col, epsilon, node_repeats=20):
-    """helper function for merge walk partitioning"""
+    """helper function for merge walk partitioning.
+    Uses :func:`~gerrychain.tree_methods.tree_part2` recursively to partition a tree into
+    ``len(parts)`` parts of population ``pop_target`` (within ``epsilon``).
+
+    :param graph: The graph
+    :param parts: Iterable of part labels (like ``[0,1,2]`` or ``range(4)``
+    :param pop_target: Target population for each part of the partition
+    :param pop_col: Node attribute key holding population data
+    :param epsilon: How far (as a percentage of ``pop_target``) from ``pop_target`` the parts
+        of the partition can be
+    :param node_repeats: Parameter for :func:`~gerrychain.tree_methods.tree_part2` to use.
+    :return: New assignments for the nodes of ``graph``.
+    :rtype: dict
+    """
     flips = {}
     remaining_nodes = set(graph.nodes)
 
     for part in parts[:-1]:
         nodes = tree_part2(
             graph.subgraph(remaining_nodes), pop_col, pop_target, epsilon, node_repeats
-        )  # should be part2
+        )
 
         for node in nodes:
             flips[node] = part
@@ -181,7 +79,7 @@ def recursive_tree_part(graph, parts, pop_target, pop_col, epsilon, node_repeats
 
         remaining_nodes -= nodes
 
-    # All remaining nodes go in the last part
+    # All of the remaining nodes go in the last part
     for node in remaining_nodes:
         flips[node] = parts[-1]
 
