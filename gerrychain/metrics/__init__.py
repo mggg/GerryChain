592ff4674c60ed01f9afb7064eb72a60e3372e68
Max Hully <max.hully@gmail.com>
2019-06-18 15:56:00 -0400

    Fix tests

--- gerrychain/metrics/__init__.py
+++ gerrychain/metrics/__init__.py
@@ -1,5 +1,11 @@
 from .compactness import polsby_popper
-from .partisan import mean_median, partisan_bias, partisan_gini, efficiency_gap
+from .partisan import (
+    mean_median,
+    partisan_bias,
+    partisan_gini,
+    efficiency_gap,
+    wasted_votes,
+)
 
 __all__ = [
     "mean_median",
@@ -7,5 +13,6 @@ __all__ = [
     "partisan_gini",
     "efficiency_gap",
     "polsby_popper",
+    "wasted_votes",
 ]
 
