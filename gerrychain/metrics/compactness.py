6897309bb61095b353ccda18aadc93fdc280119d
Abhinav Havaldar <abhinavhavaldar@Abhinavs-MacBook-Pro.local>
2020-10-23 00:03:15 -0400

    initial commit

--- gerrychain/metrics/compactness.py
+++ gerrychain/metrics/compactness.py
@@ -17,3 +17,21 @@ def polsby_popper(partition):
         )
         for part in partition.parts
     }
+
+
+def compute_schwartzberg(area, perimeter):
+    try:
+        return 2 * math.pi * math.sqrt(area / math.pi) / perimeter
+    except ZeroDivisionError:
+        return math.nan
+
+
+def schwartzberg(partition):
+    """Computes Schwartzberg compactness scores for each district in the partition.
+    """
+    return {
+        part: compute_schwartzberg(
+            partition["area"][part], partition["perimeter"][part]
+        )
+        for part in partition.parts
+    }
--- tests/metrics/test_compactness.py
+++ tests/metrics/test_compactness.py
@@ -6,3 +6,9 @@ def test_polsby_popper_returns_nan_when_perimeter_is_0():
     area = 10
     perimeter = 0
     assert compute_polsby_popper(area, perimeter) is math.nan
+
+
+def test_schwartzberg_returns_nan_when_perimeter_is_0():
+    area = 10
+    perimeter = 0
+    assert compute_schwartzberg(area, perimeter) is math.nan
