e845a68d82ae6d7c0a8c05192cffb9b1cdacd836
bsuwal <bhushan.suwal@tufts.edu>
2021-01-21 14:38:41 -0500

    add reversible recom via @pjrule (#347)
    * add reversible recom
    
    * remove superfluous import
    
    * linting fixes
    
    * more linting fixes

--- gerrychain/proposals/__init__.py
+++ gerrychain/proposals/__init__.py
@@ -1,5 +1,5 @@
 from .proposals import *
-from .tree_proposals import recom, ReCom
+from .tree_proposals import recom, reversible_recom, ReCom
 from .spectral_proposals import spectral_recom
 
-__all__ = ["recom", "spectral_recom", "propose_chunk_flip", "propose_random_flip"]
+__all__ = ["recom", "reversible_recom", "spectral_recom", "propose_chunk_flip", "propose_random_flip"]
--- gerrychain/proposals/tree_proposals.py
+++ gerrychain/proposals/tree_proposals.py
@@ -1,6 +1,10 @@
+from functools import partial
 from ..random import random
+
 from ..tree import (
-    recursive_tree_part, bipartition_tree, bipartition_tree_random
+    recursive_tree_part, bipartition_tree, bipartition_tree_random,
+    uniform_spanning_tree, find_balanced_edge_cuts_memoization,
+    BalanceError
 )
 
 
@@ -54,6 +58,68 @@ def recom(
     return partition.flip(flips)
 
 
+def reversible_recom(partition, pop_col, pop_target, epsilon,
+                     balance_edge_fn=find_balanced_edge_cuts_memoization, M=1,
+                     repeat_until_valid=False):
+    def dist_pair_edges(part, a, b):
+        return set(
+            e for e in part.graph.edges
+            if ((part.assignment[e[0]] == a and part.assignment[e[1]] == b) or
+                (part.assignment[e[0]] == b and part.assignment[e[1]] == a))
+        )
+
+    def bounded_balance_edge_fn(*args, **kwargs):
+        cuts = balance_edge_fn(*args, **kwargs)
+        if len(cuts) > M:
+            raise ReversibilityError(f'Found {len(cuts)} balance edges, '
+                                     f'but the upper bound is {M}.')
+        return cuts
+
+    bipartition_tree_random_reversible = partial(
+        bipartition_tree_random,
+        repeat_until_valid=repeat_until_valid,
+        spanning_tree_fn=uniform_spanning_tree,
+        balance_edge_fn=bounded_balance_edge_fn
+    )
+
+    parts = sorted(list(partition.parts.keys()))
+    dist_pairs = []
+    for out_part in parts:
+        for in_part in parts:
+            dist_pairs.append((out_part, in_part))
+
+    random_pair = random.choice(dist_pairs)
+    pair_edges = dist_pair_edges(partition, *random_pair)
+    if random_pair[0] == random_pair[1] or not pair_edges:
+        return partition    # self-loop: no adjacency
+
+    edge = random.choice(list(pair_edges))
+    parts_to_merge = (partition.assignment[edge[0]], partition.assignment[edge[1]])
+    subgraph = partition.graph.subgraph(
+        partition.parts[parts_to_merge[0]] | partition.parts[parts_to_merge[1]]
+    )
+
+    try:
+        flips = recursive_tree_part(
+            subgraph,
+            parts_to_merge,
+            pop_col=pop_col,
+            pop_target=pop_target,
+            epsilon=epsilon,
+            method=bipartition_tree_random_reversible
+        )
+    except BalanceError:
+        return partition    # self-loop: no balance edge
+
+    new_part = partition.flip(flips)
+    seam_length = len(dist_pair_edges(new_part, *random_pair))
+
+    if random.random() < 1 / (M * seam_length):
+        return new_part
+
+    return partition     # self-loop
+
+
 class ReCom:
     def __init__(self, pop_col, ideal_pop, epsilon, method=bipartition_tree_random):
         self.pop_col = pop_col
@@ -65,3 +131,9 @@ class ReCom:
         return recom(
             partition, self.pop_col, self.ideal_pop, self.epsilon, method=self.method
         )
+
+
+class ReversibilityError(Exception):
+    """Raised when the cut edge upper bound is violated."""
+    def __init__(self, msg):
+        self.message = msg
