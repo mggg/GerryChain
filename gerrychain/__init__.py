c86cf000e933d3a4507f584e50bd9f57866b94b6
Max Fan <root@max.fan>
2021-10-27 23:27:42 -0500

    Resolve Polygon id error

--- gerrychain/__init__.py
+++ gerrychain/__init__.py
@@ -4,5 +4,10 @@ from .graph import Graph
 from .partition import GeographicPartition, Partition
 from .updaters.election import Election
 
+import geopandas
+
+# mitigate https://github.com/geopandas/geopandas/issues/2199
+geopandas.options.use_pygeos = False
+
 __version__ = get_versions()['version']
 del get_versions
