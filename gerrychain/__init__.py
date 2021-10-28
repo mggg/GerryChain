a0ee755d014bb56e0e705c9892dfdff710a65ce6
Max Fan <root@max.fan>
2021-10-28 16:50:26 -0500

    Switch to throwing an ImportError rather than try to mitigate PyGeos issue

--- gerrychain/__init__.py
+++ gerrychain/__init__.py
@@ -6,8 +6,13 @@ from .updaters.election import Election
 
 import geopandas
 
-# mitigate https://github.com/geopandas/geopandas/issues/2199
-geopandas.options.use_pygeos = False
+# warn about https://github.com/geopandas/geopandas/issues/2199
+if geopandas.options.use_pygeos:
+    raise ImportError(
+        "GerryChain cannot use GeoPandas when PyGeos is enabled. Disable or "
+        "uninstall PyGeos. You can disable PyGeos in GeoPandas by setting "
+        "`geopandas.options.use_pygeos = False` before importing your shapefile."
+    )
 
 __version__ = get_versions()['version']
 del get_versions
