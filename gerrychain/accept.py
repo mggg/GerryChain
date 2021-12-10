089ede2859b05ddc0571decd5ec59658cbe7104b
Max Fan <root@max.fan>
2021-12-10 12:54:39 -0500

    Add type annotations for gerrychain.accept

--- gerrychain/accept.py
+++ gerrychain/accept.py
@@ -1,11 +1,19 @@
+from __future__ import annotations
+
 from .random import random
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from gerrychain.grid import Grid
+    from gerrychain.partition.partition import Partition
+    from typing import Union
 
 
-def always_accept(partition):
+def always_accept(partition: Union[Grid, Partition]) -> bool:
     return True
 
 
-def cut_edge_accept(partition):
+def cut_edge_accept(partition: Union[Grid, Partition]) -> bool:
     """Always accepts the flip if the number of cut_edges increases.
     Otherwise, uses the Metropolis criterion to decide.
 
