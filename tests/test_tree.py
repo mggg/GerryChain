72cd8e4e50067bf3bc72ae7199e2dc3e992c1a64
Matt Karrmann <mattkarrmann@gmail.com>
2023-10-03 20:12:31 -0500

    Cleaner reuse of variable

--- tests/test_tree.py
+++ tests/test_tree.py
@@ -115,7 +115,7 @@ def test_recursive_seed_part_uses_method(twelve_by_twelve_with_pop):
         ceil=None,
         method=dummy_method,
     )
-    assert calls == 7
+    assert calls == n_districts
 
 def test_random_spanning_tree_returns_tree_with_pop_attribute(graph_with_pop):
     tree = random_spanning_tree(graph_with_pop)
