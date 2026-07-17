b35bec15a68f123d7c0c860bb6967a0dc6be6782
peterrrock2 <27579114+peterrrock2@users.noreply.github.com>
2026-07-17 13:17:22 -0600

    make proposal functions

--- gerrychain/proposals/__init__.py
+++ gerrychain/proposals/__init__.py
@@ -1,5 +1,10 @@
 from .proposals import *
 from .spectral_proposals import build_spectral_recom_proposal_fn, spectral_recom
+from .multi_member_tree_proposals import (
+    MultiMemberReCom,
+    build_multi_member_recom_proposal_fn,
+    multi_member_recom,
+)
 from .tree_proposals import (
     ReCom,
     build_recom_proposal_fn,
@@ -10,12 +15,15 @@ from .tree_proposals import (
 
 __all__ = [
     "ProposalFn",
+    "MultiMemberReCom",
     "ReCom",
+    "multi_member_recom",
     "recom",
     "reversible_recom",
     "spectral_recom",
     "propose_chunk_flip",
     "propose_random_flip",
+    "build_multi_member_recom_proposal_fn",
     "build_recom_proposal_fn",
     "build_reversible_recom_proposal_fn",
 ]
